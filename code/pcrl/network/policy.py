c30e4d13893c92c5581f675a5fa3e34bdc8eaa24
lishaodong-lab <168726611+lishaodong-lab@users.noreply.github.com>
2025-11-04 10:00:12 +0800

    Add files via upload

---
+++ code/pcrl/network/policy.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+
+from .utils import build_mlp, reparameterize, evaluate_lop_pi
+
+
+class StateIndependentPolicy(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(64, 64),
+                 hidden_activation=nn.Tanh()):
+        super().__init__()
+
+        self.net = build_mlp(
+            input_dim=state_shape[0],
+            output_dim=action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+        self.log_stds = nn.Parameter(torch.zeros(1, action_shape[0]))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, states):
+        output = self.net(states)
+        # return torch.tanh(self.net(states))
+        return self.sigmoid(output)
+
+    def sample(self, states):
+        # std = torch.exp(self.log_stds)
+        return reparameterize(self.net(states), self.log_stds)
+        # return reparameterize(self.net(states), std)
+
+    def evaluate_log_pi(self, states, actions):
+        return evaluate_lop_pi(self.net(states), self.log_stds, actions)
+
+class StateFunctionDiscriminator(nn.Module):
+
+    def __init__(self, state_shape, hidden_units=(64, 64),
+                 hidden_activation=nn.Tanh()):
+        super().__init__()
+
+        self.net = build_mlp(
+            input_dim=state_shape[0],
+            output_dim=1,
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+
+    def forward(self, states):
+        return self.net(states)
+    
+
+class StateIndependentPolicy1(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(64, 64),
+                 hidden_activation=nn.Tanh()):
+        super().__init__()
+
+        self.net = build_mlp(
+            input_dim=state_shape[0],
+            output_dim=action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+        self.log_stds = nn.Parameter(torch.zeros(1, action_shape[0]))
+
+    def forward(self, states):
+        return torch.tanh(self.net(states))
+
+    def sample(self, states):
+        return reparameterize(self.net(states), self.log_stds)
+
+    def evaluate_log_pi(self, states, actions):
+        return evaluate_lop_pi(self.net(states), self.log_stds, actions)
+
+
+class StateDependentPolicy(nn.Module):
+
+    def __init__(self, state_shape, action_shape, hidden_units=(256, 256),
+                 hidden_activation=nn.ReLU(inplace=True)):
+        super().__init__()
+
+        self.net = build_mlp(
+            input_dim=state_shape[0],
+            output_dim=2 * action_shape[0],
+            hidden_units=hidden_units,
+            hidden_activation=hidden_activation
+        )
+
+    def forward(self, states):
+        return torch.tanh(self.net(states).chunk(2, dim=-1)[0])
+
+    def sample(self, states):
+        means, log_stds = self.net(states).chunk(2, dim=-1)
+        # return reparameterize(means, log_stds.clamp_(-20, 2))
+        return reparameterize(means, log_stds.clamp(-20, 2))
